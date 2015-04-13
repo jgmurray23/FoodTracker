b2e249ce2a19ea521c314642ff5e0ca9cf88fd25
jgmurray23 <joe@odell.ca>
2015-04-13 12:12:07 -0400

    commit

---
+++ HealthStoreConstant.swift
@@ -0,0 +1,15 @@
+//
+//  HealthStoreConstant.swift
+//  FoodTracker
+//
+//  Created by Joseph Murray on 2015-04-13.
+//  Copyright (c) 2015 JoeCo. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthStoreConstant {
+    let healthStore: HKHealthStore? = HKHealthStore()
+    
+}
